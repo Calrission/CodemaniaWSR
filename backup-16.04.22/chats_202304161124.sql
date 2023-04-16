a77d1c69055778c20ad34697cbfa0ade66edd56b
Kavrese <kavrese@gmail.com>
2023-04-16 11:26:53 +0300

    init backup 16.04.23

---
+++ backup-16.04.22/chats_202304161124.sql
@@ -0,0 +1,30 @@
+INSERT INTO public.chats ("firstIdUser","secondIdUser") VALUES
+	 (1,6),
+	 (2,6),
+	 (3,6),
+	 (4,6),
+	 (5,6),
+	 (1,7),
+	 (2,7),
+	 (3,7),
+	 (4,7),
+	 (5,7);
+INSERT INTO public.chats ("firstIdUser","secondIdUser") VALUES
+	 (21,1),
+	 (21,3),
+	 (21,2),
+	 (21,5),
+	 (27,1),
+	 (27,3),
+	 (27,2),
+	 (27,4),
+	 (27,5),
+	 (1,1);
+INSERT INTO public.chats ("firstIdUser","secondIdUser") VALUES
+	 (1,3),
+	 (1,2),
+	 (1,4),
+	 (26,1),
+	 (26,3),
+	 (26,2),
+	 (26,4);
---
+++ backup-16.04.22/tokens_202304161124.sql
@@ -0,0 +1,39 @@
+INSERT INTO public.tokens ("token","idUser") VALUES
+	 ('97e8e961-bb51-46dd-8165-20c83454da5e',15),
+	 ('9fbd5e06-60b0-4dc4-add2-987d9ac3f145',16),
+	 ('ed665183-96a0-4ab2-869d-05d572f37127',17),
+	 ('561de4bd-71b9-46e5-9914-4d338ffb4f96',14),
+	 ('9a73a6ec-a646-4968-9ed7-3f4db29e5beb',27),
+	 ('f3fc44df-e413-499d-985f-5dd56f0e6916',18),
+	 ('123',1),
+	 ('17aec76f-d4c6-4e2f-b15c-ab445f0f4312',20),
+	 ('c20fa5f3-6290-4553-9340-b5f4a8b946f6',19),
+	 ('38218164-3ba3-4cc4-a7c8-8199700f392c',7);
+INSERT INTO public.tokens ("token","idUser") VALUES
+	 ('8739951a-0def-4100-9d6c-3015189b29b6',28),
+	 ('124',6),
+	 ('3e7328e6-72ca-4217-954e-f4cbc1814576',29),
+	 ('36b36475-16e0-4d6d-89fb-881931864c82',30),
+	 ('76a58c4f-2167-4773-88ab-1ff3782679b3',31),
+	 ('ded0450a-f23e-4f0a-80b8-949c7ffa4c48',32),
+	 ('fb890865-ea66-4f84-802d-0eb142fd2cd0',33),
+	 ('6080f48c-8de0-44c3-899e-a82b5dc809fd',34),
+	 ('f4e78149-0986-44b3-9ccd-6fd0a3781062',35),
+	 ('02ce9fb2-b171-4e82-82a0-a223606a42aa',21);
+INSERT INTO public.tokens ("token","idUser") VALUES
+	 ('62f69a85-33de-4707-b1c2-40badc6634d2',22),
+	 ('f54508e5-8712-400b-a6de-4263fef554e8',23),
+	 ('ae41caed-9e3a-450a-bb06-96b7569c3ab3',9),
+	 ('2f4bbe88-03f6-4831-89ff-c39557b48d00',2),
+	 ('dbbe7644-fae1-4e21-ada7-d763fe178192',3),
+	 ('33cd4b48-4e0d-47fe-9db2-2633341da2de',4),
+	 ('cd69f9a1-2ec7-4748-9bce-76b6de021bb5',5),
+	 ('683f39bd-201a-42dd-9014-e17c2bb1398d',8),
+	 ('edb99dfe-5e85-4282-9504-3d2f7e66087a',10),
+	 ('03f8185c-2310-48f9-9e19-e44e059ae11a',11);
+INSERT INTO public.tokens ("token","idUser") VALUES
+	 ('68a7d01b-9062-4ebf-8032-3a453f4b883a',12),
+	 ('428bc34d-7fcd-4b4a-8f25-e4647d6bb1cf',13),
+	 ('b21888fa-9a78-487a-8949-66e0cfaed11f',24),
+	 ('03a08550-7f19-45e7-8c6c-9363cf47eb7e',25),
+	 ('319e02c4-8fef-45ca-aa0c-3691b86fad68',26);
